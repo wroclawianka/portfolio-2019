50989100dc372ab7563f0d3fa4b5b296916b1e77
dzelga <dorota.zelga@gmail.com>
2019-09-14 23:08:40 +0200

    basic structure created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Introduction from "./components/Introduction";
+import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
 
 const App = () => {
-    return <div></div>
+    return (
+       <div>
+           <Introduction/>
+           <Portfolio/>
+           <Footer/>
+       </div>
+    )
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
