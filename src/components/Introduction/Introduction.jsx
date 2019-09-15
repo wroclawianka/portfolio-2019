3363d446ba9efdc9c2a88c5d0799d72d4dd6ffc9
dzelga <dorota.zelga@gmail.com>
2019-09-15 15:23:16 +0200

    introduction finished

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -8,9 +8,11 @@ class Introduction extends Component {
                 <div className="page-title">Dorota Zelga</div>
                 <div className="content">
                     <p>
-                        I am a Front-end developer working in IT since 2014. In mid-2017 I have started my journey with
-                        frontend world, working with Angular 2+, React, JavaScript, TypeScript, jQuery and more. I am
-                        also proficient with test automation with Selenium.
+                        I am a <strong>Front-end developer</strong> working in IT since <strong>2014</strong>. In
+                        mid-2017 I have started my journey with
+                        frontend world, working with <strong>Angular
+                        2+</strong>, <strong>React</strong>, <strong>JavaScript</strong>, <strong>TypeScript</strong> and
+                        more. I am also proficient in test automation with Selenium.
                     </p>
                 </div>
                 <div className="main-picture"/>
