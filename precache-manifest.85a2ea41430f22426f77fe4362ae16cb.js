71d75bb17d0698e121cb36aa01f65dc13043e4f0
dzelga <dorota.zelga@gmail.com>
2019-09-20 20:48:11 +0200

    Updates

---
+++ precache-manifest.85a2ea41430f22426f77fe4362ae16cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c55886f7b6f3ca4af41f0c5adc3640b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "67d4c9ecab2049fad966",
+    "url": "/portfolio/static/css/main.b0b8e136.chunk.css"
+  },
+  {
+    "revision": "499110d91f44f68bd79f",
+    "url": "/portfolio/static/js/2.c9d6d7f1.chunk.js"
+  },
+  {
+    "revision": "67d4c9ecab2049fad966",
+    "url": "/portfolio/static/js/main.130fdbba.chunk.js"
+  },
+  {
+    "revision": "3ef441152abe4301ef73",
+    "url": "/portfolio/static/js/runtime~main.0476a774.js"
+  },
+  {
+    "revision": "a26b774a0a00c78c4b1ab5dfc420a6cc",
+    "url": "/portfolio/static/media/main-picture-horizontal.a26b774a.jpeg"
+  },
+  {
+    "revision": "d6160f75a30a989cee424ad061937402",
+    "url": "/portfolio/static/media/main-picture-vetrical.d6160f75.jpeg"
+  }
+]);
