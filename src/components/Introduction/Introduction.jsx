7491d941d827c45a37f4dd956a6c4e4f81d11e7d
dzelga <dorota.zelga@gmail.com>
2019-09-21 12:57:48 +0200

    intro > animations added

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -15,17 +15,19 @@ class Introduction extends Component {
                            rel="noopener noreferrer">
                             <i className="fa fa-linkedin" aria-hidden="true"/></a>
                     </div>
+                </div >
+                <div className="introduction animated animatedFadeInUp fadeInUp">
+                    <div className="content">
+                        <p>
+                            I am a <strong>Front-end developer</strong> working in IT since <strong>2014</strong>. In
+                            mid-2017 I have started my journey with
+                            frontend world, working with <strong>Angular
+                            2+</strong>, <strong>React</strong>, <strong>JavaScript</strong>, <strong>TypeScript</strong> and
+                            more. I am also proficient in test automation with Selenium.
+                        </p>
+                    </div>
+                    <div className="main-picture"/>
                 </div>
-                <div className="content">
-                    <p>
-                        I am a <strong>Front-end developer</strong> working in IT since <strong>2014</strong>. In
-                        mid-2017 I have started my journey with
-                        frontend world, working with <strong>Angular
-                        2+</strong>, <strong>React</strong>, <strong>JavaScript</strong>, <strong>TypeScript</strong> and
-                        more. I am also proficient in test automation with Selenium.
-                    </p>
-                </div>
-                <div className="main-picture"/>
             </div>
         )
     }
