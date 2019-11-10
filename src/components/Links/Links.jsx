2c7400ff5286837eecf0624e6acc4f71bd675f9b
dorota <dorota.zelga@gmail.com>
2019-11-10 19:10:37 +0100

    simplify github link

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -5,7 +5,7 @@ class Links extends Component {
     render() {
         return (
             <div className="links">
-                <a className="icon" href="https://github.com/wroclawianka/list-of-projects" target="_blank"
+                <a className="icon" href="https://github.com/wroclawianka" target="_blank"
                    rel="noopener noreferrer">
                     <i className="fa fa-github" aria-hidden="true"/></a>
                 <a className="icon" href="https://www.linkedin.com/in/dorotazelga" target="_blank"
