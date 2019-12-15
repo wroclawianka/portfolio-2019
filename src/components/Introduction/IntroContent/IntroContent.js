bd88ca22400cb5cbab6ff110257b6971c1d83fe4
Dorota Zelga <dorota.zelga@gmail.com>
2019-12-15 12:11:20 +0100

    IntroContent> component refactor

--- src/components/Introduction/IntroContent/IntroContent.js
+++ src/components/Introduction/IntroContent/IntroContent.js
@@ -2,9 +2,46 @@ import React, {Component} from "react";
 import './IntroContent.css'
 
 let options = [
-    {id: 'i_1'},
-    {id: 'i_2'},
-    {id: 'i_3'},
+    {
+        id: 'i_1', className: 'content1', renderContent: () => {
+            return (
+                <p>
+                    I am a <strong>Front-end developer</strong> working in IT
+                    since <strong>2014</strong>. In
+                    mid-2017 I have started my journey with
+                    front-end, working with <strong>Angular
+                </strong>, <strong>React</strong>, <strong>JavaScript</strong>, <strong>TypeScript</strong> and
+                    more.
+                </p>
+            )
+        }
+    },
+    {
+        id: 'i_2', className: 'content2', renderContent: () => {
+            return (
+                <p>
+                    In mattis lacus et <strong>nunc tempor</strong> condimentum. Aliquam ac turpis vel quam
+                    placerat
+                    fringilla vitae eu ante. <strong>Class aptent</strong> taciti sociosqu ad litora
+                    torquent per conubia
+                    nostra, per inceptos himenaeos
+                </p>
+            )
+        }
+    },
+    {
+        id: 'i_3', className: 'content3', renderContent: () => {
+            return (
+                <p>
+                    Duis vehicula, <strong>justo non</strong> bibendum ultrices, lacus tortor efficitur
+                    lacus, in aliquam
+                    odio lacus nec nibh. Nulla facilisis <strong>efficitur interdum</strong>. Fusce vel mi
+                    et augue mattis
+                    consectetur
+                </p>
+            )
+        }
+    },
 ];
 
 class IntroContent extends Component {
@@ -21,53 +58,34 @@ class IntroContent extends Component {
         });
     };
 
+    renderSlide = (className, renderContent) => {
+        return (
+            <div className="slide" key={"slide-" + className}>
+                <div className={"content-inner " + className}>
+                    {renderContent()}
+                </div>
+            </div>
+        )
+    };
+
     render() {
         return (
             <div className="content">
-                <div id="main">
-                    {options.map((option) => {
+                <div id="intro-slider">
+                    {options.map(({id}) => {
                         return (
-                            <input type="radio" name="switch" value={option.id} id={option.id} key={option.id}
-                                   checked={this.state.selectedOption === option.id}
+                            <input type="radio" name="switch" value={id} id={id} key={id}
+                                   checked={this.state.selectedOption === id}
                                    onChange={this.handleOptionChange}
                             />
                         )
                     })}
                     <div className="wrapper">
-                        <div className="slide">
-                            <div className="content-inner content1">
-                                <p>
-                                    I am a <strong>Front-end developer</strong> working in IT
-                                    since <strong>2014</strong>. In
-                                    mid-2017 I have started my journey with
-                                    front-end, working with <strong>Angular
-                                </strong>, <strong>React</strong>, <strong>JavaScript</strong>, <strong>TypeScript</strong> and
-                                    more.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="slide">
-                            <div className="content-inner content2">
-                                In mattis lacus et <strong>nunc tempor</strong> condimentum. Aliquam ac turpis vel quam
-                                placerat
-                                fringilla vitae eu ante. <strong>Class aptent</strong> taciti sociosqu ad litora
-                                torquent per conubia
-                                nostra, per inceptos himenaeos
-                            </div>
-                        </div>
-                        <div className="slide">
-                            <div className="content-inner content3">
-                                Duis vehicula, <strong>justo non</strong> bibendum ultrices, lacus tortor efficitur
-                                lacus, in aliquam
-                                odio lacus nec nibh. Nulla facilisis <strong>efficitur interdum</strong>. Fusce vel mi
-                                et augue mattis
-                                consectetur
-                            </div>
-                        </div>
+                        {options.map(({className, renderContent}) => this.renderSlide(className, renderContent))}
                     </div>
                     <div className="controls">
-                        {options.map((option) => {
-                            return <label htmlFor={option.id} key={option.id}>⬤</label>
+                        {options.map(({id}) => {
+                            return <label htmlFor={id} key={id}>⬤</label>
                         })}
                     </div>
                 </div>
